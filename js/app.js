12013c00aa9fdbfa8ce3eed6bac373c1363515c3
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-09 13:44:06 -0700

    Initial Sync

--- js/app.js
+++ js/app.js
@@ -11,6 +11,25 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+  	/*---Numbers---*/
+  $("#guessButton").on("click", function() {
+  	var randomNumber = Math.floor(math.random()*101);
+  	var userNumber = parseInt($('#userGuess').val(),10);
+  	var guessDifference = parseInt((randomNumber - userNumber), 10);
+  	if(guessDifference === 0) {
+  		alert("You Got It!");
+  	} else if (guessDiffernce <= 5) {
+  		alert("Very Hot!");
+  	} else if (guessDifference <=10) {
+  		alert("Hot!");
+  	} else if (guessDifference >=11) {
+  		alert("Try Again!");
+  	}
+
+  	}
+
+
+  });
 
 });
 
