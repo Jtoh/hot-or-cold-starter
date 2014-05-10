51c4cd4d20fb07ec22be77a8eb786d7795d4c563
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-09 17:00:03 -0700

    update 4-9-14

--- js/app.js
+++ js/app.js
@@ -12,24 +12,25 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
   	/*---Numbers---*/
-  $("#guessButton").on("click", function() {
-  	var randomNumber = Math.floor(math.random()*101);
+  $("#guessButton").click(function() {
+  	var randomNumber = Math.floor((math.random()*100)+1);
   	var userNumber = parseInt($('#userGuess').val(),10);
   	var guessDifference = parseInt((randomNumber - userNumber), 10);
-  	if(guessDifference === 0) {
+  	if(guessDifference >= 101 || guessDifference <0) {
+  		alert("Please enter number 1 through 100");
+  	}
+  	else if(guessDifference === 0) {
   		alert("You Got It!");
-  	} else if (guessDiffernce <= 5) {
+  	} else if (guessDifference <= 5) {
   		alert("Very Hot!");
   	} else if (guessDifference <=10) {
   		alert("Hot!");
   	} else if (guessDifference >=11) {
   		alert("Try Again!");
-  	}
-
-  	}
+  	};
 
 
-  });
+  	});
 
 });
 
