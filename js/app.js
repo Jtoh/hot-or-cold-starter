92ca5c26df6ef393f2be011f96246eeb83ea6c8e
jonathanoh0689 <jonathanoh0689@gmail.com>
2014-05-15 20:48:15 -0700

    update 4-15-14

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+      
   	/*---Numbers---*/
   $("#guessButton").click(function(e) {
     e.preventDefault();
@@ -18,18 +20,24 @@ $(document).ready(function(){
   	var userNumber = parseInt($('#userGuess').val(),10);
   	var guessDifference = parseInt((randomNumber - userNumber), 10);
   	if(isNaN(userNumber)) {
-  		alert("Please enter number 1 through 100");
+  		$('#feedback').text("Please enter number 1 through 100");
   	} else if (userNumber < 1 || userNumber > 100) {
-      alert("Please enter number 1 through 100");
+      $('#feedback').text("Please enter number 1 through 100");
     } else if(guessDifference === 0) {
-  		alert("You Got It!");
+  		$('#feedback').text("You Got It!");
   	} else if (guessDifference <= 5) {
-  		alert("Very Hot!");
+  		$('#feedback').text("Very Hot!");
   	} else if (guessDifference <=10) {
-  		alert("Hot!");
+  		$('#feedback').text("Hot!");
   	} else if (guessDifference >=11) {
-  		alert("Try Again!");
-  	};
+  		$('#feedback').text("Try Again!");
+
+      if(userNumber != randomNumber) {
+         $('#guessList').append('<li>' + userNumber +'</li>');
+    }
+  	}
+         
+   
     $('#userGuess').val(' ');
 
 
